00c8017131e9047596e118ebf56098ec7e39e201
Aidan McGeady <74266562+AidanHMcGeady@users.noreply.github.com>
2021-09-08 03:01:26 +0100

    08/09 #2

--- ModTheCube/Assets/ModTheCube/Cube.cs
+++ ModTheCube/Assets/ModTheCube/Cube.cs
@@ -9,31 +9,32 @@ public class Cube : MonoBehaviour
     public float xAngle, yAngle, zAngle;
 
     private GameObject cube1, cube2;
-    
-    static Color color1 = Color.red;
-    Color color2 = Color.green;
 
+    public Vector3 scaleChange1;
+    public Vector3 scaleChange2;
+
+    public int Num;
+
+    Color color1 = Color.red;
+    Color color2 = Color.green;
+    
     void Awake()
     {
         cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
         cube1.transform.position = new Vector3(10f, 0.0f, 0.0f);
         cube1.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
-        cube1.GetComponent<Renderer>().material.color = Color.red;
+        cube1.GetComponent<Renderer>().material.color = color1;
         cube1.name = "Self";
 
         cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
         cube2.transform.position = new Vector3(-10f, 0.0f, 0.0f);
         cube2.transform.Rotate(90.0f, 0.0f, 0.0f, Space.World);
-        cube2.GetComponent<Renderer>().material.color = Color.green;
+        cube2.GetComponent<Renderer>().material.color = color2;
         cube2.name = "World";
 
-        InvokeRepeating("RotateCubes", 1f, 0.05f);
+        InvokeRepeating("RotateCubes", 1f, Random.Range(0.01f, 0.25f));
         InvokeRepeating("ChangeColor", 1f, 1f);
-    }
-
-    void Update()
-    {
-
+        InvokeRepeating("ScaleCube", 1f, 1f);
     }
 
     void RotateCubes()
@@ -43,12 +44,12 @@ public class Cube : MonoBehaviour
         zAngle = Random.Range(0, 15);
 
         cube1.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
-        cube2.transform.Rotate(xAngle, yAngle, zAngle, Space.World);
+        cube2.transform.Rotate(xAngle, yAngle, zAngle, Space.World);  
     }
 
     void ChangeColor()
-    {
-        int Num = Random.Range(0, 5);
+    {      
+        Num = Random.Range(0, 5);
 
         if(Num == 0)
         {
@@ -80,5 +81,17 @@ public class Cube : MonoBehaviour
             color1 = Color.green;
             color2 = Color.blue;
         }
+
+        cube1.GetComponent<Renderer>().material.color = color1;
+        cube2.GetComponent<Renderer>().material.color = color2;
+    }
+
+    void ScaleCube()
+    {
+        scaleChange1 = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+        scaleChange2 = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+
+        cube1.transform.localScale = scaleChange1;
+        cube2.transform.localScale = scaleChange2;
     }
 }
