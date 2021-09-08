50d3869d00d3b9d90ba0b9e4b5886ab17f2146b6
Aidan McGeady <74266562+AidanHMcGeady@users.noreply.github.com>
2021-09-08 01:53:24 +0100

    Initial commit

---
+++ A New Start/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public int moveSpeed;
+    public int jumpForce;
+    public int baseDamage;
+
+    int Coins;
+    int Gems;
+    int Souls;
+
+    bool isCrouched;
+    bool isGrounded;
+    bool hasAttacked;
+    bool 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ModTheCube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    public float xAngle, yAngle, zAngle;
+
+    private GameObject cube1, cube2;
+    
+    static Color color1 = Color.red;
+    Color color2 = Color.green;
+
+    void Awake()
+    {
+        cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube1.transform.position = new Vector3(10f, 0.0f, 0.0f);
+        cube1.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+        cube1.GetComponent<Renderer>().material.color = Color.red;
+        cube1.name = "Self";
+
+        cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube2.transform.position = new Vector3(-10f, 0.0f, 0.0f);
+        cube2.transform.Rotate(90.0f, 0.0f, 0.0f, Space.World);
+        cube2.GetComponent<Renderer>().material.color = Color.green;
+        cube2.name = "World";
+
+        InvokeRepeating("RotateCubes", 1f, 0.1f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void RotateCubes()
+    {
+        xAngle = Random.Range(0, 15);
+        yAngle = Random.Range(0, 15);
+        zAngle = Random.Range(0, 15);
+
+        cube1.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+        cube2.transform.Rotate(xAngle, yAngle, zAngle, Space.World);
+    }
+
+    void ChangeColor()
+    {
+
+    }
+}
